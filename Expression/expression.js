c8ec2dde38c561a4c2920f58cb4f3fff6831957b
nettosz <nettodiniz3d@gmail.com>
2022-05-28 00:40:09 -0300

    pull

--- Expression/expression.js
+++ Expression/expression.js
@@ -2,3 +2,15 @@ function MakeExpression(element, operation)
 {
 
 }
+
+// Gera as cadeias para cada palavra, a, aa,aaa
+function genExp(word)
+{
+    let list_wds = []
+    for (i=1; i<=5 ; i++) {
+        list_wds.push(word.repeat(i))
+    }
+    return list_wds
+}
+
+console.log(genExp("a"))
