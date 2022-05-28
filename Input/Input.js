d79e60943cf5f81155d362ce8b9c0f86bc859db9
Breno <Breno>
2022-05-27 23:05:59 -0300

    refatorando e adicionando função para analisar expressão simples

--- Input/Input.js
+++ Input/Input.js
@@ -1,5 +1,5 @@
-const expressionParentheses = /(\([\w+\s*\+*\s*\w*\s*]+\)(\^\+)?\*?)/i; // expressão para pegar paranteses
-const normalExpression = /(\w+((\^\+)?\*?)\s*)*/i; // Normal expression
+const expressionParentheses = /(\([\w+\s*\+*\s*\w*\s*]+\)(\^\+)?\*?)/i; // Expressões compostas, ex: (ab + c), (ab + (b + c))*, etc...
+const normalExpression = /(\w+((\^\+)?\*?)\s*)*/i; // Expressões normais, ex: ab*
 
 const expressions = []; // Para guardar as expressões com parenteses
 
@@ -12,23 +12,27 @@ function input(input){
   }
 }
 
-let txt = "(ab + c )(ab + (ab + (ab + c)))^+ + (ab)* + a";
+let txt = "(ab + c)(ab + (ab + (ab + c)))^+ + (ab)* + a";
 
-// Para substituir as expressões encontradas por "exp"
+// Função recursiva para substituir as expressões do tipo (ab + b) e dar o push para expressions
 function replacementParentheses(input){
   if(typeof input != 'string'){
     return -1;
   }
 
+  // se nao encontrar expressões compostas, chama a função para pegar expressões do tipo ab*, etc...
   if(input.search(expressionParentheses) == -1){
+    replacementNormal(input);
     return;
   }
 
+  // se encontrar expressões do tipo composta, irá dar o push 
   expressions.push(input.match(expressionParentheses)[0]);
 
   return replacementParentheses(input.replace(expressionParentheses, "exp"));
 }
 
+// Função recursiva para pegar expressões sozinhas ex: ab*, só será chamada quando não for encontrada expressões do tipo composta.
 function replacementNormal(input){
   if(typeof input != 'string'){
     return -1;
@@ -38,6 +42,11 @@ function replacementNormal(input){
     return;
   }
 
+  input = input.replace(/exp/ig, "").replace(/\+/ig, "").trim(); 
+  expressions.push(input.match(normalExpression)[0]);
+  console.log(input);
+
+  return replacementNormal(input);
 }
 
 replacementParentheses(txt);
