647e8be431d2db977445e1eeda6bb6e33a90664a
nettosz <nettodiniz3d@gmail.com>
2022-05-31 18:47:18 -0300

    a

--- Expression/expression.js
+++ Expression/expression.js
@@ -3,59 +3,114 @@ const opRepeatOnceOrMore = /(\^\+)$/i;
 const opRepeat = /(\*)$/i;
 const opChanger = /\s+\+\s+/ig;
 
-const expressionsToMake = [];
-let txt = "01*";
+let txt = "(a + b)a*";
 
 const replacementParentheses = require('../Input/Input');
-console.log(replacementParentheses(txt)[0]);
-console.log(clearExpression(replacementParentheses(txt)[0]));
+
+let replacedExps = replacementParentheses(txt)
+let expressionsToMake = []
+
+for (let i = 0; i < replacedExps.length; i++) {
+    expressionsToMake.push(clearExpression(replacedExps[i]))
+}
+
+console.log(genExp(expressionsToMake))
 
 // Gera as cadeias para cada palavra, a, aa,aaa
-function genExp(word)
+function genExpSingle(keep, word)
 {
     let list_wds = []
-    for (i=1; i<=5 ; i++) {
-        list_wds.push(word.repeat(i))
+    if(!keep)
+    {
+        for (i=1; i<=4 ; i++) {
+            list_wds.push(word.repeat(i))
+        }
+    } else {
+        for (i=1; i<=4 ; i++) {
+            list_wds.push(keep + word.repeat(i))
+        }
     }
     return list_wds
 }
 
-function genPlus(exp)
+function genExpComposite(keep, listWords)
 {
-    
-}
-
-function genMult(exp)
-{
-
-}
-
-function isRepeatOnceMore(expression){  
-    if(!testExpression(opRepeatOnceOrMore, expression)){
-        return false;
+    let list_wds = []
+    for (i=1; i<=4 ; i++)
+    {
+        if(typeof listWords === "object")
+        {
+            listWords.map(item => {
+                list_wds.push(item.repeat(i))
+            })
+        } else {
+            if (!keep)
+            {
+                list_wds.push(listWords.repeat(i))
+            } else {
+                list_wds.push(keep + listWords.repeat(i))
+            }
+        }
     }
-
-    //  Irá retirar o sinal, e os parenteses restando as funções
-    console.log(`repetir a expressão ${expression} pelo menos um ou mais vezes`);
+    return list_wds
 }
 
-function isRepeat(expression){
-    if(!testExpression(opRepeat, expression)){
-        return false;
+function genExp(exp)
+{   
+    full_exps = []
+    for (let i = 0; i < exp.length; i++) {
+        if(!exp[i].operator[0])
+        {
+            let templist = []
+            if(typeof exp[i].expression[0] == "object")
+            {
+                exp[i].expression[0].map(item => {
+                    templist.push(item)
+                })
+            } else {
+                templist.push(exp[i].expression[0])
+            }
+            full_exps.push(templist)
+        }
+        
+        if(exp[i].operator[0] === "*")
+        {
+            if(typeof exp[i].expression[0] === "object")
+            {
+                full_exps.push(genExpComposite("", exp[i].expression[0]))
+            }
+            else
+            {
+                if(!exp[i].operator[1]){
+                    full_exps.push(genExpComposite("",exp[i].expression[0]))
+                } else {
+                    full_exps.push(genExpComposite(keep=exp[i].expression[0][0],exp[i].expression[0][1]))
+                }
+            }
+        }
+        
+        if(exp[i].operator[0] === "^+")
+        {
+            if(exp[i].operator[1])
+            {
+                full_exps.push(genExpSingle("", exp[i].expression[0]))
+            }
+            else
+            {
+                full_exps.push(genExpSingle(keep=exp[i].expression[0][0], exp[i].expression[0][1]))
+            }
+        } 
     }
-
-    console.log(`repetir a ${expression} nenhuma ou muita vezes`);
+    return full_exps
 }
-
 
 function clearExpression(input){
     let expression; 
     let alternator, operator, repeatAll = false; 
 
     testExpression(opChanger, input[0]) ? alternator = true : alternator = false;
-    testExpression(opRepeatOnceOrMore, input[0]) ? operator = "^+": operator = ""; 
-    testExpression(opRepeat, input[0]) ? operator="*" : operator = ""; 
-
+    testExpression(opRepeatOnceOrMore, input[0]) ? operator = "^+":  testExpression(opRepeat, input[0]) ? operator="*" : operator = "";  
+    
     if(operator == "^+"){
         if(testExpression(parentheses, input[0])){
             repeatAll = true;
@@ -75,7 +130,7 @@ function clearExpression(input){
         input[0] = input[0].split('+');
     }   
 
-
+    
     return {
         expression: [input[0], input[1]],
         operator: [operator, repeatAll]
